fb5c60fce5b219361b91186ac46ed3dbe795b02c
Somtochukwu <63365931+somT-oss@users.noreply.github.com>
2020-07-06 19:40:15 +0100

    Create Simple-Interest-Calculator.java

---
+++ Simple-Interest-Calculator.java
@@ -0,0 +1,31 @@
+package com.codewithme;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Build {
+    public static void main(String[] args) {
+        NumberFormat number = NumberFormat.getCurrencyInstance();
+        NumberFormat secondNumber = NumberFormat.getPercentInstance();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your principal amount: ");
+        float principal = scanner.nextInt();
+
+        System.out.print("Enter the time duration of the amount: ");
+        float time = scanner.nextInt();
+        float maineTime = time * 12;
+
+        System.out.print("Enter rate: ");
+        float rate = scanner.nextInt();
+        float mainRate = rate / 100;
+
+        float simpleInterest =  principal * mainRate * maineTime / 100;
+        String mainSimpleInterest = number.format(simpleInterest);
+
+        System.out.println("Note that the time will be recorded in months so 2 years == 24 months " +
+                "and the rate will be measured in percentage like 15% == .15 ");
+        System.out.println("The simple interest is: " + mainSimpleInterest);
+
+    }
+}
